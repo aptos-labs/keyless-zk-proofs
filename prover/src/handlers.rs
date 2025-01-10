30837cca17b7699b197bc3b85c4f40814135c8fa
zhoujun.ma <zhoujun@aptoslabs.com>
2025-01-10 12:17:15 -0800

    update

--- prover/src/handlers.rs
+++ prover/src/handlers.rs
@@ -153,7 +153,10 @@ pub async fn prove_handler(
         ON_CHAIN_TW_PK.read().unwrap().as_ref(),
         state.tw_keypair_new.as_ref(),
     ) {
-        (Some(on_chain), Some(local)) if on_chain.data.training_wheels_pubkey == &local.on_chain_repr.data.training_wheels_pubkey => {
+        (Some(on_chain), Some(local))
+            if on_chain.data.training_wheels_pubkey
+                == local.on_chain_repr.data.training_wheels_pubkey =>
+        {
             (true, &local.signing_key, &local.verification_key)
         }
         _ => (
