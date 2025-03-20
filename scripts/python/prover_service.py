3ecd31ea6f2b934b853240fd6127193c1238ee86
Rex Fernando <rex1fernando@gmail.com>
2025-03-20 01:03:07 +0000

    debugging stuff

--- scripts/python/prover_service.py
+++ scripts/python/prover_service.py
@@ -5,6 +5,6 @@ def install_deps():
     manage_deps.install_deps(["pkg-config", "lld", "meson", "rust", "clang", "cmake", "make", "libyaml", "nasm", "gmp", "openssl", "nlohmann-json"])
     
 def add_envvars_to_profile():
-    path = utils.repo_root() + "/rust-rapidsnark/rapidsnark/build/subprojects/oneTBB-2022.0.0"
-    utils.add_envvar_to_profile("LD_LIBRARY_PATH", "$LD_LIBRARY_PATH:" + path)
-    utils.add_envvar_to_profile("DYLD_LIBRARY_PATH", "$DYLD_LIBRARY_PATH:" + path)
+    path = utils.repo_root() / "rust-rapidsnark/rapidsnark/build/subprojects/oneTBB-2022.0.0"
+    utils.add_envvar_to_profile("LD_LIBRARY_PATH", "$LD_LIBRARY_PATH:" + str(path))
+    utils.add_envvar_to_profile("DYLD_LIBRARY_PATH", "$DYLD_LIBRARY_PATH:" + str(path))
