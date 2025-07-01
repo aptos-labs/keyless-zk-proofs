68e26c099d27adcfbf88e8098d773defa554ab25
zhoujun.ma <zhoujun@aptoslabs.com>
2025-07-01 01:56:43 -0700

    update

--- scripts/python/setups/testing_setup.py
+++ scripts/python/setups/testing_setup.py
@@ -57,7 +57,7 @@ class TestingSetup(setups.Setup):
         start_time = time.time()
         utils.run_shell_command('echo hihi')
         utils.run_shell_command('find ~ -name circomlib')
-        utils.run_shell_command('echo J005613; set -x; . ~/.nvm/nvm.sh; npm root -g')
+        utils.run_shell_command('echo J005613; ls -la .venv/bin; . ~/.nvm/nvm.sh; npm root -g')
         utils.run_shell_command('echo hoho')
         utils.run_shell_command('circom -l . -l $(export; set -x; . ~/.nvm/nvm.sh; npm root -g) main.circom --r1cs --wasm --c --sym')
         eprint("Compilation took %s seconds" % (time.time() - start_time))
