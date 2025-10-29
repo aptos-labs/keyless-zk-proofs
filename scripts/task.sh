a22c37e38d89a2db145b541095b8c2c49c129fdb
Josh Lind <josh@aptoslabs.com>
2025-10-29 18:26:47 -0400

    [Lint] Add lint script (with new rust verifications) to CI/CD. (#77)

--- scripts/task.sh
+++ scripts/task.sh
@@ -4,7 +4,40 @@ set -ex
 
 SCRIPT_DIR=$(dirname "$0")
 
+# Installs rustup if not already installed
+function install_rustup {
+  VERSION="$(rustup --version || true)"
+  if [ -n "$VERSION" ]; then
+    if [[ "${BATCH_MODE}" == "false" ]]; then
+      echo "Rustup is already installed, version: $VERSION"
+    fi
+  else
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
+    if [[ -n "${CARGO_HOME}" ]]; then
+      PATH="${CARGO_HOME}/bin:${PATH}"
+    else
+      PATH="${HOME}/.cargo/bin:${PATH}"
+    fi
+  fi
+}
+
+# Installs cargo-machete if not already installed
+function install_cargo_machete {
+  if ! command -v cargo-machete &>/dev/null; then
+    cargo install cargo-machete --locked --version 0.7.0
+  fi
+}
+
+# Installs cargo-sort if not already installed
+function install_cargo_sort {
+  if ! command -v cargo-sort &>/dev/null; then
+    cargo install cargo-sort --locked --version 1.0.7
+  fi
+}
+
+# Installs necessary dependencies
 install_deps() {
+  # Install python3 and curl if not present
   if ! command -v python3 > /dev/null || ! command -v curl > /dev/null; then
     OS=$(uname -s)
     case $OS in
@@ -43,6 +76,13 @@ install_deps() {
     esac
     >&2 echo "Dependencies installation finished."
   fi
+
+  # Install rustup
+  install_rustup
+
+  # Install cargo dependency tools
+  install_cargo_machete
+  install_cargo_sort
 }
 
 install_deps
