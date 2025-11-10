367553209bfb6f99a0f3c53b83665978bce8c35d
Josh Lind <josh.lind@hotmail.com>
2025-11-10 17:10:12 -0500

    [Prover] Small fixes to onchain keyless config.

--- prover-service/src/prover_key.rs
+++ prover-service/src/prover_key.rs
@@ -7,11 +7,35 @@ use serde::{Deserialize, Serialize};
 #[cfg(test)]
 use std::io::Write;
 
+
+// TODO: merge this with the same struct used by the pepper service
+
+/// This struct is a representation of an OnChainKeylessConfiguration resource as found on-chain.
+/// See, for example:
+/// https://fullnode.testnet.aptoslabs.com/v1/accounts/0x1/resource/0x1::keyless_account::Configuration
+///
+/// Example JSON:
+/// {
+///  "type": "0x1::keyless_account::Configuration",
+///  "data": {
+///    "max_commited_epk_bytes": 93,
+///    "max_exp_horizon_secs": "10000000",
+///    "max_extra_field_bytes": 350,
+///    "max_iss_val_bytes": 120,
+///    "max_jwt_header_b64_bytes": 300,
+///    "max_signatures_per_txn": 3,
+///     "override_aud_vals": [],
+///     "training_wheels_pubkey": {
+///       "vec": [
+///         "0x1388de358cf4701696bd58ed4b96e9d670cbbb914b888be1ceda6374a3098ed4"
+///       ]
+///     }
+///   }
+/// }
 #[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq)]
 pub struct OnChainKeylessConfiguration {
-    /// Some type info returned by node API.
-    pub r#type: String,
-    pub data: ConfigData,
+    pub r#type: String, // Note: "type" is a reserved keyword, so we use raw identifier syntax
+    pub data: KeylessConfigurationData,
 }
 
 impl OnChainKeylessConfiguration {
@@ -24,7 +48,7 @@ impl OnChainKeylessConfiguration {
 
         Self {
             r#type: "0x1::keyless_account::Configuration".to_string(),
-            data: ConfigData {
+            data: KeylessConfigurationData {
                 max_commited_epk_bytes: 93,
                 max_exp_horizon_secs: "10000000".to_string(),
                 max_extra_field_bytes: 350,
@@ -38,8 +62,9 @@ impl OnChainKeylessConfiguration {
     }
 }
 
+/// The data fields of the OnChainKeylessConfiguration resource
 #[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq)]
-pub struct ConfigData {
+pub struct KeylessConfigurationData {
     pub max_commited_epk_bytes: u16,
     pub max_exp_horizon_secs: String,
     pub max_extra_field_bytes: u16,
@@ -50,6 +75,7 @@ pub struct ConfigData {
     pub training_wheels_pubkey: TrainingWheelsPubKey,
 }
 
+/// The training wheels public key of the OnChainKeylessConfiguration resource
 #[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq)]
 pub struct TrainingWheelsPubKey {
     vec: Vec<String>,
