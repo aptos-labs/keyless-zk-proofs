8c2e817380a384d03108780b3983366369b17aea
Josh Lind <josh.lind@hotmail.com>
2025-11-19 17:16:41 -0500

    [Prover Service] Clean up witness gen file and logic.

--- keyless-common/src/input_processing/witness_gen.rs
+++ keyless-common/src/input_processing/witness_gen.rs
@@ -1,20 +1,9 @@
 // Copyright (c) Aptos Foundation
 
 use anyhow::{anyhow, bail, Result};
+use std::path::Path;
 use std::{fs, process::Command};
 use tempfile::NamedTempFile;
-
-pub trait PathStr {
-    fn path_str(&self) -> Result<&str>;
-}
-
-impl PathStr for NamedTempFile {
-    fn path_str(&self) -> Result<&str> {
-        self.path()
-            .to_str()
-            .ok_or_else(|| anyhow!("tempfile path error"))
-    }
-}
 
 pub fn witness_gen(
     witness_gen_js_path: &str,
@@ -24,13 +13,18 @@ pub fn witness_gen(
     let input_file = NamedTempFile::new()?;
     let witness_file = NamedTempFile::new()?;
     fs::write(input_file.path(), body.as_bytes())?;
+
+    let input_file_path = get_file_path_string(input_file.path())?;
+    let witness_file_path = get_file_path_string(witness_file.path())?;
+
     let mut cmd = get_witness_command(
         witness_gen_js_path,
         witness_gen_wasm_path,
-        input_file.path_str()?,
-        witness_file.path_str()?,
+        &input_file_path,
+        &witness_file_path,
     );
     let output = cmd.output()?;
+
     // Check if the command executed successfully
     if output.status.success() {
         Ok(witness_file)
@@ -40,6 +34,14 @@ pub fn witness_gen(
         let stdout = String::from_utf8_lossy(&output.stdout);
         bail!("Command failed:\n{}\n{}", stdout, stderr)
     }
+}
+
+/// Converts a file path to a string, returning an error if the conversion fails
+pub fn get_file_path_string(file_path: &Path) -> Result<String> {
+    let file_path_string = file_path
+        .to_str()
+        .ok_or_else(|| anyhow!("Failed to convert file path to string: {:?}!", file_path))?;
+    Ok(file_path_string.to_string())
 }
 
 fn get_witness_command(
