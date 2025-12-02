37ea2c6366c65c056ecf5939a43af25e49c94eb8
Josh Lind <josh.lind@hotmail.com>
2025-12-02 18:04:10 -0500

    [Prover Service] Clean up to test utils.

--- prover-service/src/tests/utils.rs
+++ prover-service/src/tests/utils.rs
@@ -7,75 +7,48 @@ use rsa::rand_core;
 use rsa::traits::PublicKeyParts;
 use std::str::FromStr;
 
-const TEST_JWK_EXPONENT_STR: &str = "65537";
-
+/// A simple RSA public key representation (for testing)
 #[derive(Debug, PartialEq, Eq)]
 pub struct RsaPublicKey {
     modulus: BigUint,
 }
 
 impl RsaPublicKey {
-    #[allow(clippy::all)]
-    pub fn from_str(s: &str) -> anyhow::Result<Self, anyhow::Error> {
-        Ok(Self {
-            modulus: BigUint::from_str(s)?,
-        })
-    }
-
-    pub fn to_64bit_limbs(&self) -> Vec<u64> {
-        self.modulus.to_u64_digits()
-    }
-
-    pub fn to_bytes(&self) -> Vec<u8> {
-        self.modulus.to_bytes_be()
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        Self {
-            modulus: BigUint::from_bytes_be(bytes),
-        }
-    }
-
-    // TODO test from and as below
-    pub fn from_mod_b64(modulus_b64: &str) -> anyhow::Result<Self, anyhow::Error> {
-        let modulus_bytes = base64::decode_config(modulus_b64, base64::URL_SAFE_NO_PAD)?;
-        Ok(RsaPublicKey::from_bytes(&modulus_bytes))
-    }
-
+    /// Returns the modulus as a base64 string
     pub fn as_mod_b64(&self) -> String {
         base64::encode_config(self.modulus.to_bytes_be(), base64::URL_SAFE_NO_PAD)
     }
 }
 
+/// A simple RSA private key representation (for testing)
 pub struct RsaPrivateKey {
     internal_private_key: rsa::RsaPrivateKey,
 }
 
 impl RsaPrivateKey {
+    /// Generates a new RSA private key with the given bit size and public exponent
     pub fn new_with_exp<R>(
         rng: &mut R,
         bit_size: usize,
         exp: &BigUint,
     ) -> anyhow::Result<Self, anyhow::Error>
     where
-        R: rsa::rand_core::CryptoRngCore + ?Sized,
+        R: rand_core::CryptoRngCore + ?Sized,
     {
         let exp_rsa_type = rsa::BigUint::from_bytes_be(&exp.to_bytes_be());
+        let internal_private_key = rsa::RsaPrivateKey::new_with_exp(rng, bit_size, &exp_rsa_type)?;
         Ok(Self {
-            internal_private_key: rsa::RsaPrivateKey::new_with_exp(rng, bit_size, &exp_rsa_type)?,
+            internal_private_key,
         })
     }
 
+    /// Returns the encoding key for this private key (for JWT signing)
     pub fn as_encoding_key(&self) -> jsonwebtoken::EncodingKey {
-        let encoding_key = jsonwebtoken::EncodingKey::from_rsa_pem(
-            self.internal_private_key
-                .to_pkcs1_pem(LineEnding::LF)
-                // this fn is only used in tests, so unwrapping is ok
-                .unwrap()
-                .as_bytes(),
-        )
-        .unwrap();
-        encoding_key
+        let pkcs1_pem = self
+            .internal_private_key
+            .to_pkcs1_pem(LineEnding::LF)
+            .unwrap();
+        jsonwebtoken::EncodingKey::from_rsa_pem(pkcs1_pem.as_bytes()).unwrap()
     }
 }
 
@@ -94,7 +67,7 @@ pub fn generate_test_jwk_keypair() -> impl TestJWKKeyPair {
 
 /// Generates a test JWK keypair with the given KID
 pub fn generate_test_jwk_keypair_with_kid(kid: &str) -> impl TestJWKKeyPair {
-    let exp = BigUint::from_str(TEST_JWK_EXPONENT_STR).unwrap();
+    let exp = BigUint::from_str("65537").unwrap();
     let mut rng = rand_core::OsRng;
 
     DefaultTestJWKKeyPair::new_with_kid_and_exp(&mut rng, kid, exp).unwrap()
